3f200252ec3973a9b7e675ab134393e8b208434d
Ginajunian <gina10222200@sttcipasung.ac.id>
2026-09-04 21:35:20 +0700

    fix path logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,4 +1,4 @@
 <div class="flex items-center gap-2">
-    <img src="{{ asset('img/logo_MTs.png') }}" alt="Logo"
+    <img src="{{ asset('img/Logo_MTs.png') }}" alt="Logo"
         style="width: 40px; height: auto; margin-right: 10px; bg-white rounded-full;">
 </div>
